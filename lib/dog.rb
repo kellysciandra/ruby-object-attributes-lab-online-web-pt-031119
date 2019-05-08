d9ea75ae7a2d05bdd022724e85e3566f84c5d5ff
Kelly Sciandra <kellysciandradev@icloud.com>
2019-05-08 14:24:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,17 @@ class Dog
   def name= (full_name)
     @name= full_name
   end 
-    def name 
-      @name 
-    end 
+    
+  def name 
+    @name 
+  end 
   
   def breed= (full_breed)
     @breed= full_breed
   end 
   
-    def breed
-     @breed 
+  def breed
+    @breed 
   end 
   
 end 
